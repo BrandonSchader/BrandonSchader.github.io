6946d6cf53555562c71bc5d62bc9cad848f8e760
Brandon-Schader-Student <100796870+BrandonSchader@users.noreply.github.com>
2023-03-14 13:08:03 -0400

    all

---
+++ script/menu1.js
@@ -0,0 +1,20 @@
+function openNav() 
+{
+  document.getElementById("mySidenav").style.width = "250px";
+}
+
+function closeNav() 
+{
+  document.getElementById("mySidenav").style.width = "0";
+}
+
+function myFunction() 
+{
+  var header = document.getElementById("home");
+  var sticky = header.offsetTop;
+  if (window.pageYOffset > sticky) {
+    header.classList.add("sticky");
+  } else {
+    header.classList.remove("sticky");
+  }
+}
